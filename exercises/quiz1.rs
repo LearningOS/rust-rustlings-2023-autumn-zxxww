f8fa08645813d55f3e3753ca9c7d905c8b91c780
zxxww <2023534990@qq.com>
2023-10-10 23:35:34 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(m:i32)->i32 {
+    if m<=40{
+        m*2
+    }else{
+        m*1
+    }
+
+}
 
 // Don't modify this function!
 #[test]
